69e76095cca0706b83780b3354e8c44ca2c360e9
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2024-10-18 22:53:33 +0300

    Code Refactoring

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const extentionIconPaths: Record<string, string> = {
+  // Files
+  tsx: "/icons/react_ts",
+  jsx: "/icons/react",
+  html: "/icons/html",
+  svg: "/icons/svg",
+  //Folders
+  node_modules: "/icons/folder-node",
+  src: "/icons/folder-src",
+  public: "/icons/folder-public",
+  components: "/icons/folder-components",
+  images: "/icons/folder-images",
+};
