42615013f01ba94b4b23487a28dd99061b01b960
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2024-11-09 20:59:13 +0200

    done

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
-import fileTreeSlice from './features/tree/fileTreeSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import fileTree from './features/tree/fileTreeSlice'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-
-export const store = configureStore({
-  reducer: {
-    fileTree: fileTreeSlice,
-  },
+const fileTreePersistConfig = {
+  key: "fileTree",
+  storage,
+}
+const rootReducer = combineReducers({
+  fileTree: persistReducer(fileTreePersistConfig, fileTree)
+})
+const store = configureStore({
+  reducer: rootReducer,
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+const persistor = persistStore(store)
+export { store, persistor };
